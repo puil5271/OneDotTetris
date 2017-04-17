49b5ed5d851b749cd8ab95b66c08c305f690ed47
puil5271 <puil5271@gmail.com>
2017-04-17 17:21:34 -0600

    Upload Files

---
+++ OneBlockTetris/OneDotTetris.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System.Drawing;
+
+namespace OneBlockTetris
+{
+    partial class OneDotTetris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.blockPanel = new System.Windows.Forms.Panel();
+            this.blockLabels = new System.Windows.Forms.Label[40];
+            this.SuspendLayout();
+            // 
+            // blockPanel
+            // 
+            this.blockPanel.Location = new System.Drawing.Point(12, 12);
+            this.blockPanel.Name = "blockPanel";
+            this.blockPanel.Size = new System.Drawing.Size(350, 500);
+            this.blockPanel.TabIndex = 0;
+            this.blockPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            //
+            //  create the labels
+            //
+            int blockNumber = 0;
+            int blockPositionX = 0;
+            int blockRow = 0;
+
+            foreach(System.Windows.Forms.Label block in blockLabels)
+            {
+                blockLabels[blockNumber] = new System.Windows.Forms.Label();
+                this.blockLabels[blockNumber].BackColor = Color.LimeGreen;
+                this.blockLabels[blockNumber].Location = new System.Drawing.Point(60 * blockPositionX, 60 * blockRow);
+                this.blockLabels[blockNumber].Name = "blockLabel" + blockNumber.ToString();
+                this.blockLabels[blockNumber].Size = new System.Drawing.Size(50, 50);
+                this.blockLabels[blockNumber].TabIndex = blockNumber;
+
+                blockNumber++;
+                blockPositionX++;
+
+                if (blockNumber == 5)
+                {
+                    blockRow++;
+                    blockPositionX = 0;
+                }
+                if (blockNumber == 10)
+                {
+                    blockRow++;
+                    blockPositionX = 0;
+                }
+                if (blockNumber == 15)
+                {
+                    blockRow++;
+                    blockPositionX = 0;
+                }
+                if (blockNumber == 20)
+                {
+                    blockRow++;
+                    blockPositionX = 0;
+                }
+                if (blockNumber == 25)
+                {
+                    blockRow++;
+                    blockPositionX = 0;
+                }
+                if (blockNumber == 30)
+                {
+                    blockRow++;
+                    blockPositionX = 0;
+                }
+                if (blockNumber == 35)
+                {
+                    blockRow++;
+                    blockPositionX = 0;
+                }
+                if (blockNumber == 40)
+                {
+                    blockRow++;
+                    blockPositionX = 0;
+                }
+            }
+
+            blockNumber = 0;
+            foreach (System.Windows.Forms.Label block in blockLabels)
+            {
+                this.blockPanel.Controls.Add(this.blockLabels[blockNumber]);
+                blockNumber++;
+            }
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 500);
+            this.Controls.Add(this.blockPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel blockPanel;
+
+        private System.Windows.Forms.Label[] blockLabels;
+
+        public System.Windows.Forms.Label[] BlockLabels
+        {
+            get { return blockLabels; }
+            set { blockLabels = value; }
+        }
+
+    }
+}
+
---
+++ OneBlockTetris/OneDotTetris.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OneBlockTetris
+{
+    public partial class OneDotTetris : Form
+    {
+        private Color[] _blockEmptyColor;
+
+        private Timer _timer;
+
+        private int _tetrisPiecePosition;
+        private bool[] _blocksFilled;
+
+        public OneDotTetris()
+        {
+            InitializeComponent();
+
+            _timer = new Timer();
+            _timer.Interval = 150;
+            _timer.Enabled = true;
+            _timer.Tick += new System.EventHandler(TimerTickEvent);
+
+            _blockEmptyColor = new Color[2];
+
+            _blocksFilled = new bool[40];
+
+            _tetrisPiecePosition = -3;
+
+            _blockEmptyColor[0] = Color.LimeGreen;
+
+            _blockEmptyColor[1] = Color.DarkSlateBlue;
+
+            foreach (Label label in BlockLabels)
+            {
+                label.BackColor = _blockEmptyColor[0];
+            }
+
+            _timer.Start();
+        }
+
+        private void TimerTickEvent(object sender, EventArgs e)
+        {
+            _tetrisPiecePosition += 5;
+
+            if (_tetrisPiecePosition < 35)
+            {
+                if (BlockLabels[_tetrisPiecePosition].BackColor == _blockEmptyColor[1])
+                {
+                    _tetrisPiecePosition = 2;
+                }
+            }
+
+            if (_tetrisPiecePosition == 2)
+            {
+                int amount = 0;
+                foreach (Label block in BlockLabels)
+                {
+                    if (block.BackColor == _blockEmptyColor[1])
+                    {
+                        _blocksFilled[amount] = true;
+                    }
+                    amount++;
+                }
+            }
+
+            if (_tetrisPiecePosition > 40)
+            {
+                _tetrisPiecePosition = 2;
+            }
+            if (_tetrisPiecePosition < 40)
+            {
+                if (BlockLabels[_tetrisPiecePosition].BackColor == _blockEmptyColor[0])
+                {
+                    BlockLabels[_tetrisPiecePosition].BackColor = _blockEmptyColor[1];
+                    if (_tetrisPiecePosition > 5)
+                    {
+                        BlockLabels[_tetrisPiecePosition - 5].BackColor = _blockEmptyColor[0];
+                    }
+                }
+            }
+
+            if (BlockLabels[35].BackColor == _blockEmptyColor[1] &&
+                BlockLabels[36].BackColor == _blockEmptyColor[1] &&
+                BlockLabels[37].BackColor == _blockEmptyColor[1] &&
+                BlockLabels[38].BackColor == _blockEmptyColor[1] &&
+                BlockLabels[39].BackColor == _blockEmptyColor[1])
+            {
+                //  Free the filled line
+                BlockLabels[35].BackColor = _blockEmptyColor[0];
+                BlockLabels[36].BackColor = _blockEmptyColor[0];
+                BlockLabels[37].BackColor = _blockEmptyColor[0];
+                BlockLabels[38].BackColor = _blockEmptyColor[0];
+                BlockLabels[39].BackColor = _blockEmptyColor[0];
+
+                int amountBlocks = 0;
+
+                //  Make all _blockFilled false
+                _blocksFilled = new bool[40];
+
+                //  Sets ones that are blue to true
+                foreach (Label block in BlockLabels)
+                {
+                    if (block.BackColor == _blockEmptyColor[1])
+                    {
+                        _blocksFilled[amountBlocks + 5] = true;
+                    }
+                    amountBlocks++;
+                }
+
+                //  Make all pieces empty
+                foreach (Label block in BlockLabels)
+                {
+                    block.BackColor = _blockEmptyColor[0];
+                }
+
+                //  Make the pieces fall down after the Line is freed
+                int amountBlue = 0;
+                foreach (bool blockFilled in _blocksFilled)
+                {
+                    if (blockFilled == true)
+                    {
+                        BlockLabels[amountBlue].BackColor = _blockEmptyColor[1];
+                    }
+                    amountBlue++;
+                }
+
+                if (_tetrisPiecePosition < 40)
+                {
+                    if (blockLabels[_tetrisPiecePosition].BackColor == _blockEmptyColor[0])
+                    {
+                        BlockLabels[_tetrisPiecePosition].BackColor = _blockEmptyColor[1];
+                        if (_tetrisPiecePosition > 5)
+                        {
+                            BlockLabels[_tetrisPiecePosition - 5].BackColor = _blockEmptyColor[0];
+                        }
+                    }
+                }
+            }
+
+            // When filled Blocks reach the top
+            if (BlockLabels[2].BackColor == _blockEmptyColor[1] && BlockLabels[7].BackColor == _blockEmptyColor[1])
+            {
+                _timer.Stop();
+                DialogResult quitOrContinue = MessageBox.Show("You Lost. Do you want to restart the Game?", "Fail", MessageBoxButtons.YesNo);
+
+                if (quitOrContinue == DialogResult.Yes)
+                {
+                    foreach (Label block in BlockLabels)
+                    {
+                        block.BackColor = _blockEmptyColor[0];
+                    }
+                    _blocksFilled = new bool[40];
+                    _tetrisPiecePosition = -3;
+                    _timer.Start();
+                }
+                else
+                {
+                    Environment.Exit(0);       
+                }
+            }
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Left)
+            {
+                if (_tetrisPiecePosition < 39 && _blocksFilled[_tetrisPiecePosition] == false)
+                {
+                    if (_tetrisPiecePosition != 0 && _tetrisPiecePosition != 5 && _tetrisPiecePosition != 10 && _tetrisPiecePosition != 15
+                         && _tetrisPiecePosition != 20 && _tetrisPiecePosition != 25 && _tetrisPiecePosition != 30 && _tetrisPiecePosition != 35
+                         && BlockLabels[_tetrisPiecePosition - 1].BackColor != _blockEmptyColor[1])
+                    {
+                        _tetrisPiecePosition--;
+                        BlockLabels[_tetrisPiecePosition].BackColor = _blockEmptyColor[1];
+                        BlockLabels[_tetrisPiecePosition + 1].BackColor = _blockEmptyColor[0];
+                        return true;
+                    };
+                }
+            }
+
+            if (keyData == Keys.Right)
+            {
+                if (_tetrisPiecePosition < 39 && _blocksFilled[_tetrisPiecePosition] == false)
+                {
+                    if (_tetrisPiecePosition != 0 && _tetrisPiecePosition != 4 && _tetrisPiecePosition != 9 && _tetrisPiecePosition != 14
+                         && _tetrisPiecePosition != 19 && _tetrisPiecePosition != 24 && _tetrisPiecePosition != 29 && _tetrisPiecePosition != 34
+                         && BlockLabels[_tetrisPiecePosition + 1].BackColor != _blockEmptyColor[1])
+                    {
+                        _tetrisPiecePosition++;
+                        BlockLabels[_tetrisPiecePosition].BackColor = _blockEmptyColor[1];
+                        BlockLabels[_tetrisPiecePosition - 1].BackColor = _blockEmptyColor[0];
+                        return true;
+                    };
+                }
+            }
+           
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
